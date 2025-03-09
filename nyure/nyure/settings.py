a34c3403e10747fd634d0549a7ad4e0e88373f10
Rohan Neure <114942902+neurerohan@users.noreply.github.com>
2025-03-09 19:21:44 +0545

    Update settings.py

--- nyure/nyure/settings.py
+++ nyure/nyure/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#*1iz$2v@)&1p!ya(=crzm37cnm9nu(@0t^g9vnuw%=5injw*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nyure.onrender.com', 'api.kalimatirate.nyure.com.np']
 
